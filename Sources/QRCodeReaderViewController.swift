b7fb99662cb5027b6e1a8b53437305b37c9999c9
Yegor Terokhin <yegor.terokhin@anywhere.cz>
2017-07-07 14:10:18 +0200

    Changed privacy access in vc from public -> open

--- Sources/QRCodeReaderViewController.swift
+++ Sources/QRCodeReaderViewController.swift
@@ -28,9 +28,9 @@ import UIKit
 import AVFoundation
 
 /// Convenient controller to display a view to scan/read 1D or 2D bar codes like the QRCodes. It is based on the `AVFoundation` framework from Apple. It aims to replace ZXing or ZBar for iOS 7 and over.
-public class QRCodeReaderViewController: UIViewController {
+open class QRCodeReaderViewController: UIViewController {
   /// The code reader object used to scan the bar code.
-  public let codeReader: QRCodeReader
+  open let codeReader: QRCodeReader
 
   let readerView: QRCodeReaderContainer
   let startScanningAtLoad: Bool
@@ -43,10 +43,10 @@ public class QRCodeReaderViewController: UIViewController {
   // MARK: - Managing the Callback Responders
 
   /// The receiver's delegate that will be called when a result is found.
-  public weak var delegate: QRCodeReaderViewControllerDelegate?
+  open weak var delegate: QRCodeReaderViewControllerDelegate?
 
   /// The completion blocak that will be called when a result is found.
-  public var completionBlock: ((QRCodeReaderResult?) -> Void)?
+  open var completionBlock: ((QRCodeReaderResult?) -> Void)?
 
   deinit {
     codeReader.stopScanning()
@@ -112,11 +112,11 @@ public class QRCodeReaderViewController: UIViewController {
   }
 
   // MARK: - Responding to View Events
-  override public var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+  override open var supportedInterfaceOrientations: UIInterfaceOrientationMask {
     return parent?.supportedInterfaceOrientations ?? .all
   }
 
-  override public func viewWillAppear(_ animated: Bool) {
+  override open func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
 
     if startScanningAtLoad {
@@ -124,13 +124,13 @@ public class QRCodeReaderViewController: UIViewController {
     }
   }
 
-  override public func viewWillDisappear(_ animated: Bool) {
+  override open func viewWillDisappear(_ animated: Bool) {
     stopScanning()
 
     super.viewWillDisappear(animated)
   }
 
-  override public func viewWillLayoutSubviews() {
+  override open func viewWillLayoutSubviews() {
     super.viewWillLayoutSubviews()
 
     codeReader.previewLayer.frame = view.bounds
@@ -188,12 +188,12 @@ public class QRCodeReaderViewController: UIViewController {
   // MARK: - Controlling the Reader
 
   /// Starts scanning the codes.
-  public func startScanning() {
+  open func startScanning() {
     codeReader.startScanning()
   }
 
   /// Stops scanning the codes.
-  public func stopScanning() {
+  open func stopScanning() {
     codeReader.stopScanning()
   }
 
